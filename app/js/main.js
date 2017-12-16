2740fa320d71a76b3eca593b5245700cf191af04
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-12-16 10:04:16 -0600

    Added google tag manager

--- app/js/main.js
+++ app/js/main.js
@@ -103,6 +103,8 @@
 
 }(window));
 
+(function(w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, 'script', 'dataLayer', 'GTM-5878F3K');
+
 window.addEventListener('load', function () {
 	window.app.init();
 });
